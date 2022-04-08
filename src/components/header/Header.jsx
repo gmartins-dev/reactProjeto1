19b29860b36b652e39daa68ae1d7436f024ee5db
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-08 15:28:41 -0300

    fim segunda aula

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,30 @@
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
 import styles from './Header.module.css'
+import Icon from "../icon/Icon"
+
+
+
 
 function Header (){
 
     return(
         <div className={styles.header}>
-        <Title>Tic Tac Toe Game</Title>
-        <Subtitle>Made using ReactJS</Subtitle>
-        <Subtitle>By @guilhermemm-dev</Subtitle>
+            <Title>Tic Tac Toe Game</Title>
+            <Subtitle>Made using ReactJS</Subtitle>
+            <Subtitle>By @guilhermemm-dev</Subtitle>
+            <div className={styles.iconContent}>
+                <a><Icon 
+                iconName="github"
+                link="https://github.com/guilhermemm-dev"
+                /></a>
+            </div>
         </div>
+
+
+
+
+
     )
 
 }
---
+++ src/components/icon/Icon.jsx
@@ -0,0 +1,22 @@
+
+
+function Icon ({iconName, size, link}) {
+
+    return(
+
+        <a href={link} target="_blank">
+            <img src={`/icons/${iconName}.svg`} width={size} />
+        </a>
+
+    )
+
+}
+
+Icon.defaultProps = {
+
+    size: '30px'
+
+}
+
+
+export default Icon
