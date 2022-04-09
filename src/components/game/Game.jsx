7a48ea0cc9bcf1223405f72a55ba05f1df4e0714
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-09 07:08:30 -0300

    criado estados no gameoption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,30 @@
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
+import { useState } from 'react'
+
+
 
 
 function Game () {
 
+    const [gameState, setGameState] = useState(Array(9).fill(0))
+    console.log (gameState)
     return(
 
         <div className={styles.game}>
-            <GameOption />
-            <GameOption />
-            <GameOption />
+            {
+                gameState.map(
+                    (value, pos) =>
+                    <GameOption 
+                    
+                        key={`game-option-pos-${pos}`}
+                        status={value}
+                    
+                    />
+                )
+                
 
-            <GameOption />
-            <GameOption />
-            <GameOption />
-
-            <GameOption />
-            <GameOption />
-            <GameOption />          
+            }       
         </div>
 
     )
